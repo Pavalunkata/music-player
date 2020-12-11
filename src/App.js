92d6cce928e76cf0ce64a30296013d54cfed331b
Pavalunkata <pavkata.staikov@gmail.com>
2020-12-11 08:30:09 +0200

    stalys and data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
 //Adding stayles
 import stayle from './stayle/stayle.scss';
+//Import Songs
+import data from './util';
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0])
   return (
     <div className="App">
-      <Song />
+      <Song currentSong = {currentSong} />
       <Player />
     </div>
   );
